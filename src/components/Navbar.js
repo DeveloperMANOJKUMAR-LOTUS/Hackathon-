a3c9a8906611c9c6c50027dcaa867b210e4693c3
DeveloperMANOJKUMAR-LOTUS <developermanoj2003@gmail.com>
2022-10-19 09:37:56 +0530

    hello world

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
 import Logo from '../assets/images/Logo.png';
+
+
 
 const Navbar = () => (
   <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
@@ -18,6 +20,16 @@ const Navbar = () => (
     >
       <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid blue' }}>Home</Link>
       <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
+      {/* <Link to="/Doctors" style={{ textDecoration: 'none', color: '#3A1212' }}>Doctors</Link>
+      <Link to="/Hospitals" style={{ textDecoration: 'none', color: '#3A1212' }}>Hospitals</Link> */}
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path='/doctors' element={<Doctors />} ></Route>
+          <Route path='/hospitals' element={<Hospitals />} ></Route>
+        </Routes>
+      </BrowserRouter> */}
+      
+      
     </Stack>
   </Stack>
 );
